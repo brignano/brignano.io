38ffc2d80db41d984f2c71912c80faf8ce473514
brignano <abrignano@comcast.net>
2026-01-18 23:58:29 -0500

    feat: convert GA_MEASUREMENT_ID to string for consistent type handling

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,4 +1,4 @@
-export const GA_MEASUREMENT_ID = process.env.GA_MEASUREMENT_ID;
+export const GA_MEASUREMENT_ID = String(process.env.GA_MEASUREMENT_ID);
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: string) => {
